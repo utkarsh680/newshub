9996ebc5a78faca0afffefd76a6907c75b207432
utkarsh <singhrishu680@gmail.com>
2023-02-13 17:25:51 +0530

    add constructor and make state

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 export default class Navbar extends Component {
-    static propTypes = {
-        prop: PropTypes
-    }
+    
 
     render() {
         return (
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,16 +2,18 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
 
+   
+
     render() {
-       let  {title, description} = this.props;
+       let  {title, description, imageurl, newsUrl} = this.props;
         return (
             <div>
                 <div className="card" style={{width:"18rem"}}>
-                    <img className="card-img-top" src="https://live-production.wcms.abc-cdn.net.au/4273843cf270710bb72e589d6c8581b3?impolicy=wcms_crop_resize&cropH=1458&cropW=2592&xPos=0&yPos=243&width=862&height=485" alt="Card image cap" />
+                    <img className="card-img-top" src={imageurl} alt="Card image cap" />
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text">{description}</p>
-                        <a href="/newsdetail" className="btn btn-primary">Go somewhere</a>
+                        <a href="/newsdetail" className="btn btn-sm btn-primary">Read more</a>
                     </div>
                 </div>
             </div>
