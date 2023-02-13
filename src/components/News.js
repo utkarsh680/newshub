7e54c4fc8863c199d876249d3772745ef873fccd
utkarsh <singhrishu680@gmail.com>
2023-02-13 20:26:30 +0530

    work for next and previous button

--- src/components/News.js
+++ src/components/News.js
@@ -21,6 +21,35 @@ export class News extends Component {
             articles:parsedData.articles
         })
     }
+    handlePrevClick = async()=>{
+        console.log("previous");
+        let url = `https://newsapi.org/v2/top-headlines?country=in&category&apiKey=cfa7ffe3855542638258bb6c87591da2=${this.state.page - 1}`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        console.log(parsedData);
+     
+        this.setState({
+            page: this.state.page - 1,
+            articles:parsedData.articles
+
+        })
+
+
+    }
+    handleNextClick = async ()=>{
+        console.log("next");
+        let url = `https://newsapi.org/v2/top-headlines?country=in&category&apiKey=cfa7ffe3855542638258bb6c87591da2=${this.state.page + 1}`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        console.log(parsedData);
+     
+        this.setState({
+            page: this.state.page + 1,
+            articles:parsedData.articles
+
+        })
+
+    }
     render() {
         return (
             <div className='container my-3'>
@@ -36,8 +65,8 @@ export class News extends Component {
                     
                 </div>
                 <div className='container d-flex justify-content-between my-3'>
-                <button type="button" class="btn btn-outline-dark">previous</button>
-                <button type="button" class="btn btn-outline-dark">next</button>
+                <button disabled= {this.state.page <=1} type="button" class="btn btn-outline-dark" onClick={this.handlePrevClick}> &larr; previous</button>
+                <button type="button" class="btn btn-outline-dark" onClick={this.handleNextClick}>next &#8594;</button>
 
                 </div>
             </div>
